1fce475436c8e16a6ab5c96c776468f095009986
GUAYA96 <luis.trujillo.@outlook.com>
2023-01-13 12:49:19 -0600

    Semana uno lmtm

---
+++ uno12/com/Palindromo.java
@@ -0,0 +1,31 @@
+package com;
+
+import java.util.Scanner;
+
+public class Palindromo {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println();
+		String original = sc.nextLine();
+		original = original.replace("","");
+		System.out.println(original);	
+				
+		String reversa = "";
+
+		// Leyendo la cadena al reves
+		for (int i = original.length() - 1; i >= 0; i--) {
+			reversa = reversa + Character.toString(original.charAt(i));
+
+		}
+		if (original.equals(reversa)) {
+			System.out.println("Si es palindromo");
+
+		} else {
+			System.out.println("No es palindromo ");
+
+		}
+		original.equalsIgnoreCase((original));
+	}
+}
---
+++ uno12/com/ejercicio10.java
@@ -0,0 +1,41 @@
+package com;
+
+import java.util.Scanner;
+
+public class ejercicio10 {
+	public static void main(String[] args) {
+
+		int contador = 0;
+		Scanner sc = new Scanner(System.in);
+		int num = 0;
+		int sumPositivos = 0;
+		int sumImpares = 0;
+
+		while (contador < 20) {
+			System.out.println("Ingresa un numero: [" + contador + "]");
+
+			num = sc.nextInt();
+						
+			if(num>=0) {
+				System.out.println("positivos");
+				//Calcula sumatoria positivos
+				sumPositivos = sumPositivos + num;
+				
+			}else {
+				System.out.println("Negativo");
+				
+			}
+
+			if(num%2==0){
+				System.out.println("Es par");
+			}else {
+				System.out.println("Impar");
+				sumImpares = sumImpares + num;
+			}
+			
+			System.out.println("Sumatoria positivos: "+sumPositivos);
+			System.out.println("Sumatoria impares); "+sumImpares);
+		}
+		}
+	}
+
