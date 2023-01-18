150f955fa2bfa6a8b37223d6d93ee8cbee1fe89d
Dann1902 <dan.aguilar1902@gmail.com>
2023-01-18 10:55:15 -0600

    ClasesAbstractas

---
+++ ClasesAbstractas/src/com/ICientificas.java
@@ -0,0 +1,11 @@
+package com;
+
+//Todo los metodos dentro de una interface son abstractos
+//Son publicos por default
+public interface ICientificas {
+	double raizCuadrada(double num);
+	double seno(double num);
+	
+	
+
+}
