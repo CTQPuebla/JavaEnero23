89db9ba90a42917b06b384ad97137b0e61d59b5d
PabloR31 <pfrojas31@gmail.com>
2023-02-03 12:55:26 -0600

    CRUDHibernate

---
+++ ClasesAbstractas/src/com/ICientificas.java
@@ -0,0 +1,14 @@
+package com;
+// Todos los metodos dentro de una interface son abtractos
+//Son públicos por default
+
+public interface ICientificas {
+	double raizCuadrada(double num);
+	double seno(double num);
+	
+	
+	
+	
+	
+
+}
---
+++ ClasesAbstractas/src/com/IOtrasOperaciones.java
@@ -0,0 +1,12 @@
+package com;
+
+public interface IOtrasOperaciones {
+	
+	int metodA();
+	void metodB (int a);
+	String metodC(int x, String y);
+	
+	
+
+
+}
